412ffd73cb4647e59369d6bab6e7d4933e192705
mightyjol <jerome.hanke@gmail.com>
2019-04-08 16:20:09 +0200

    Update prestashopbridge.php

--- prestashopbridge.php
+++ prestashopbridge.php
@@ -88,6 +88,7 @@ class PrestashopBridge extends Module
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // TODO figure out if this even matters?
         curl_exec($ch);
         curl_close($ch);
     }
